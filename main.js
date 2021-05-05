35ae2bda17d31c1d77377044967dc8b66e9c6d18
vj9898 <81862570+vj9898@users.noreply.github.com>
2021-05-05 11:14:09 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+var firstname="ved";
+console.log(firstname);
+
+var lastname="joshi";
+console.log(lastname);
+
+var fullname=firstname.concat(lastname);
+console.log=(fullname);
+
+var lengthfullname=fullname.length;
+console.log(fullname);
+
+var squareroot=math.sqrt(16)
+
+var squareroot=math.sqrt(30)
